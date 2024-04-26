12ed7351cf4580125c050dbd12441b1f92b5d29d
Debraj-Das <debrajdas.gayatri@gmail.com>
2024-04-27 03:05:45 +0530

    add diagonostic in none-ls and add code action in virtual status

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,20 +23,17 @@ return {
       lspconfig.tsserver.setup({
         capabilities = capabilities
       })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
+		lspconfig.ruff_lsp.setup({
+			capabilities = capabilities
+		})
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
 }
