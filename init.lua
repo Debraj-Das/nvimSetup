7c7283c1a851991c82e8e5269b3fe407ccdb7a63
Debraj-Das <debrajdas.gayatri@gmail.com>
2024-05-14 12:55:52 +0530

    create a common reprojectory for nvim setting

--- init.lua
+++ init.lua
@@ -1,22 +1,2 @@
--- set the leader key
-vim.g.mapleader = " "
-
--- lazy package manager setup
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
-
-
--- Author basic setup, remap, .. etc 
-require("setting")
+-- Debraj Das setup, remap, .. etc 
+require("debraj")
--- lua/debraj/keybinding.lua
+++ lua/debraj/keybinding.lua
@@ -1,18 +1,19 @@
+-- set the leader key
+vim.g.mapleader = " "
+
 -- Mapping the key
-vim.keymap.set('n', '<TAB>', '%', { noremap = true })
-vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true })
+vim.keymap.set({'n', 'v'}, '<TAB>', '%', { noremap = true })
 
 -- Normal mode key bindings
 vim.keymap.set('n', '<CR>', 'o<esc>', { noremap = true })
-vim.keymap.set('n', '<C-n>', ':tabnew<CR>', { noremap = true })
-vim.keymap.set('n', '<C-Down>', ':m+1<CR>', { noremap = true })
-vim.keymap.set('n', '<C-Up>', ':m-2<CR>', { noremap = true })
+vim.keymap.set('n', '<C-j>', ':m+1<CR>', { noremap = true })
+vim.keymap.set('n', '<C-k>', ':m-2<CR>', { noremap = true })
 vim.keymap.set('n', 'd', '"_d', { noremap = true })
 vim.keymap.set('n', '<BS>', '"_xh', { noremap = true })
 
 -- Visual mode key bindings
-vim.keymap.set('v', '<C-Down>', ':m\'>+1<CR>gv=gv', { noremap = true })
-vim.keymap.set('v', '<C-Up>', ':m\'<-2<CR>gv=gv', { noremap = true })
+vim.keymap.set('v', '<C-j>', ':m\'>+1<CR>gv=gv', { noremap = true })
+vim.keymap.set('v', '<C-k>', ':m\'<-2<CR>gv=gv', { noremap = true })
 vim.keymap.set('v', '<', '<gv', { noremap = true })
 vim.keymap.set('v', '>', '>gv', { noremap = true })
 vim.keymap.set('v', 'd', '"_d', { noremap = true })
@@ -20,18 +21,15 @@ vim.keymap.set('v', 'd', '"_d', { noremap = true })
 -- Insert mode key bindings
 vim.keymap.set('i', '<C-j>', '<Esc>o', { noremap = true })
 vim.keymap.set('i', '<C-h>', '<C-w>', { noremap = true })
-vim.keymap.set('i', '<C-Down>', '<esc>:m+1<CR>', { noremap = true })
-vim.keymap.set('i', '<C-Up>', '<esc>:m-2<CR>', { noremap = true })
 vim.keymap.set('i', '{<CR>', '{<CR>}<esc>ko', { noremap = true })
 
--- Other mappings
-vim.keymap.set('n', '<Leader>j', '<esc>:tabprevious<CR>', { noremap = true })
-vim.keymap.set('n', '<Leader>k', '<esc>:tabnext<CR>', { noremap = true })
+-- Leader key mappings
+vim.keymap.set('n', '<Leader>n',  vim.cmd.tabnew, { noremap = true })
+vim.keymap.set('n', '<Leader>q',  vim.cmd.q, {noremap = true })
+vim.keymap.set('n', '<Leader>j',	 vim.cmd.tabprevious, { noremap = true })
+vim.keymap.set('n', '<Leader>k',	 vim.cmd.tabnext, { noremap = true })
+vim.keymap.set('n', '<Leader>s',  vim.cmd.w, { noremap = true })
 vim.keymap.set('n', '<Leader>y', '<esc>:%y+<CR>', { noremap = true })
 vim.keymap.set('n', '<Leader>v', '<C-v>', { noremap = true })
 vim.keymap.set('n', '<Leader>r', '<C-r>', { noremap = true })
-vim.keymap.set('n', '<Leader>s',  vim.cmd.w, { noremap = true })
-vim.keymap.set('n', '<Leader>q',  vim.cmd.q, {noremap = true })
 vim.keymap.set('n', '<Leader>h', ':set hlsearch!<CR>', { noremap = true })
-
-
