7f31444217ea9dd09be022df79c4834a32321895
Debraj-Das <debrajdas.gayatri@gmail.com>
2024-05-20 02:37:32 +0530

    add surround plugin in nvim setup

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,15 +1,10 @@
 return {
 	{
 		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"hrsh7th/cmp-buffer",
-	},
-	{
-		"hrsh7th/cmp-path",
-	},
-	{
-		"hrsh7th/cmp-cmdline",
+		'hrsh7th/cmp-path',
+		'hrsh7th/cmp-cmdline',
+		'hrsh7th/nvim-cmp',
+		'hrsh7th/cmp-buffer'
 	},
 	{
 		"L3MON4D3/LuaSnip",
@@ -42,31 +37,29 @@ return {
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
 					{ name = "buffer" },
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
 				}),
 			})
 
-			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline({ "/", "?" }, {
+			cmp.setup.cmdline({ '/', '?' }, {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = {
-					{ name = "buffer" },
-				},
+					{ name = 'buffer' }
+				}
 			})
 
-			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline(":", {
+			cmp.setup.cmdline(':', {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
+					{ name = 'path' }
+					}, {
+						{ name = 'cmdline' }
 				}),
-				matching = { disallow_symbol_nonprefix_matching = false },
+				matching = { disallow_symbol_nonprefix_matching = false }
 			})
 		end,
 	},
 }
+
